d997375c9b0c7bf61f4337095c88df97d4cf1845
root <root@localhost.localdomain>
2026-08-18 09:43:46 +0700

    Mengubah mysql url

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,11 +2,7 @@ import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  process.env.MYSQL_PUBLIC_URL
 });
 
 export default pool;
